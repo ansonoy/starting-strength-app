c3cfbd4ca1c4e5d5f9b004683028614172aea51d
Anson <ansonoy00@gmail.com>
2025-11-11 21:14:44 -0700

    Reset the app plus added boiler plate

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native"
+
+export default function Index() {
+  // Check authentication
+  // if isAuthenticated
+  // go to home page
+  // else go to sign in page
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-sky-800 text-3xl">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  )
+}
